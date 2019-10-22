ed7c7c6b4488cf8147a42fe90944b40da2589911
Minauras <remi.clerc@epfl.ch>
2019-10-22 15:37:53 +0200

    Did feature selection and added new data load function

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # The_Little_Turings
 
-Test 1
-Test 2 (Jo)
+List of files:
+- Exploratory Data Analysis: identification of the features of the dataset and their properties
+- Feature Selection: Analysis of the score of meaning of each feature
+- Optimizing learning: ...
+- Optimizing LR: ...
+- project1: ...
+
+- implementations: required function for the project handout
+- misc helpers: helper functions that don't fit in any other helper category
+- ml_math: some mathematical functions needed for ML applications
+- plot functions: some useful pre-written plots
+- proj1 helpers: provided helper functions for project 1
--- misc_helpers.py
+++ misc_helpers.py
@@ -4,8 +4,29 @@
 ###############################################################
 
 import numpy as np
+from proj1_helpers import *
 
-
+def load_reduced_training_data(data_path, delete_low_output_correlation=True, delete_high_feature_correlation=True):
+    """
+    This function will load the training data, with the possibility to remove features according to our results of Feature Selection
+    """
+    y, tX, ids = load_csv_data(data_path)
+    
+    features_low_output_correlation = [2, 7, 8, 14, 15, 16, 17, 18, 19, 20]
+    features_high_feature_correlation = [5, 11, 23, 24, 25, 26, 27]
+    
+    if delete_low_output_correlation and delete_high_feature_correlation:
+        tX_reduced = np.delete(tX, features_low_output_correlation + features_high_feature_correlation, axis=1)
+    elif delete_high_feature_correlation:
+        tX_reduced = np.delete(tX, features_high_feature_correlation, axis=1)
+    elif delete_low_output_correlation:
+        tX_reduced = np.delete(tX, features_low_output_correlation, axis=1)
+    else:
+        tX_reduced = np.copy(tX)
+    
+    return y, tX_reduced, ids
+    
+    
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
     """
     Generate a minibatch iterator for a dataset.
